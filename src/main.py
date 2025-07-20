6f93f841220c51e75fdd374bf38853dc4b0f8381
Adam Hernandez <ahernandez411@gmail.com>
2025-07-20 13:53:12 -0600

    add extra payment logic

--- src/main.py
+++ src/main.py
@@ -14,22 +14,36 @@ class Main:
         self.loan_amount = loan_terms.amount
         self.loan_rate = loan_terms.rate
         self.loan_months = loan_terms.months
+        self.large_payments = loan_terms.large_payments
+        self.down_payment = loan_terms.down_payment
 
 
     def run(self):
+        loan_stats = {}
         print("")
         print("#############################################")
         print("Loan Payment Simluator")
         print("#############################################")
 
         # Initial Values
-        remaining_balance = self.loan_amount
+        remaining_balance = self.loan_amount - self.down_payment
         annual_rate = self.loan_rate / 100
         months = self.loan_months
 
+        print(f"Loan Amount before Down Payment: ${self.loan_amount:.2f}")
         print(f"Loan Amount: ${remaining_balance:.2f}")
+        print(f"Down Payment: ${self.down_payment:.2f}")
         print(f"Annual Rate: {self.loan_rate}%")
         print(f"Loan Months: {months}")
+
+        loan_stats["base"] = {
+            "amount-before-down-payment": f"{self.loan_amount:.2f}",
+            "amount-after-down-payment": f"{remaining_balance:.2f}",
+            "amount-down-payment": f"{self.down_payment:.2f}",
+            "annual-percentage-rate": f"{self.loan_rate:.2f}",
+            "months-maximum": months
+        }
+        loan_stats["payment-months"] = {}
 
 
         # Convert annual rate to monthly
@@ -42,30 +56,57 @@ class Main:
         monthly_payment = remaining_balance * fraction
         
         print(f"Monthly Payment ${monthly_payment:.2f}")
+        loan_stats["monthly-payment"] = monthly_payment
         
         total_interest = 0
-        for month_number in range(self.loan_months):
+        for index in range(self.loan_months):
+            month_number = index + 1
+
+            extra_payment = 0
+            if self.large_payments and month_number in self.large_payments:
+                extra_payment = self.large_payments[month_number]
+
             print("")
-            print(f"Payment Amounts on Month {month_number + 1}")
+            print(f"Payment Amounts on Month {month_number}")
 
             interest = remaining_balance * monthly_rate
             total_interest += interest
             principal = monthly_payment - interest
+            if extra_payment > 0:
+                print(f"Extra Large Payment: ${extra_payment:.2f}")
+                principal = extra_payment - interest
+
             principal_interest = interest + principal
             
             remaining_balance = remaining_balance - principal
             remaining_balance = max(0, remaining_balance)
+
+            if remaining_balance == 0:
+                print(f"- Loan is paid off in {month_number-1} months!")
+                loan_stats["base"]["months-actual"] = (month_number-1)
+                break
 
             print(f"- Payment Amount: ${principal_interest:.2f}")
             print(f"   - Principal: ${principal:.2f}")
             print(f"   - Interest: ${interest:.2f}")
             print(f"- Remaining: ${remaining_balance:.2f}")
 
+            loan_stats["payment-months"][month_number] = {
+                "amount": f"${principal_interest:.2f}",
+                "interest": f"${interest:.2f}",
+                "principal": f"${principal:.2f}",
+                "remaining": f"${remaining_balance:.2f}"
+            }
+            if extra_payment > 0:
+                loan_stats["payment-months"][month_number]["extra-payment"] = True
+
         print(f"Total Interest Paid: ${total_interest:.2f}")
+        loan_stats["base"]["total-interest-paid"] = f"${total_interest:.2f}"
+
+        with open("files/loan-details.json", "w") as writer:
+            json.dump(loan_stats, writer, indent=3, sort_keys=True)
 
 
         
-
-
 if __name__ == "__main__":
     Main().run()
